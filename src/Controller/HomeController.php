dc80ab8d86f5a3efdf17ec5c1a59054c4ab43b57
mohamedkante <114979734+mohamedkante@users.noreply.github.com>
2023-07-31 19:44:06 +0200

    connexion & authentication users

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name:'basepage')]
+    public function base(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
